897ae3e9745797edb9cdaecb79e81ad3354cdd44
tuttlepower <tatuttle2015@gmail.com>
2020-04-30 16:56:49 -0400

    Create predictit_markets.py

---
+++ predictit_markets.py
@@ -0,0 +1,16 @@
+# df creator using predictits csv downloads that are available
+import pandas as pd 
+
+def data(market):
+    #24h,7d,30d, 90d,
+    time = '90d'
+    url = 'https://www.predictit.org/Resource/DownloadMarketChartData?marketid='+str(market)+'&timespan='+str(time)
+    df = pd.read_csv(url)
+    return df
+    
+def market_name():
+    # this may not be the best way to do it, but it worked pretty well for me 
+    df = pd.read_json('https://www.predictit.org/api/marketdata/markets/6598')
+    # sets the market to a string and returns it. 
+    text = str(df['name'][0])
+    return text
